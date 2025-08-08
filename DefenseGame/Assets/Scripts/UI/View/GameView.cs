db8efddc0cd7613db3c8ba62389723583a5d5fed
의수 이 <ocm13180@gmail.com>
2025-08-09 02:08:46 +0900

    플레이어 및 몬스터 프리펩 & 더미 Ground 및 Tile

--- DefenseGame/Assets/Scripts/UI/View/GameView.cs
+++ DefenseGame/Assets/Scripts/UI/View/GameView.cs
@@ -4,45 +4,35 @@ using UnityEngine.UI;
 
 public class GameView : UIView
 {
-    // [SerializeField] Joystick joystick;
-    // [SerializeField] Button attackBtn;
-    // [SerializeField] Button homeBtn;
-
-    // private PlayerInfo playerInfo;
-
-    // public void Show()
-    // {
-    //     ShowLayer();
-    // }
-    // protected override void OnFirstShow()
-    // {
-    //     playerInfo = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInfo>();
-    //     attackBtn.onClick.AddListener(OnClick_AttackBtn);
-
-    //     // homeBtn.onClick.AddListener(OnClick_HomeBtn);
-    // }
-    // protected override void OnShow()
-    // {
-    //     playerInfo._playerMoveControl.SetJoystick(joystick);
-    //     Init();
-    // }
-
-    // private void Init()
-    // {
-    // }
-
-    // #region Event
-    // private void OnClick_AttackBtn()
-    // {
-    //     // SoundManager.Instance.PlaySFXSound("Button");
-    //     playerInfo._playerAniControl.Attack();
-    // }
-    // private void OnClick_HomeBtn()
-    // {
-    //     // SoundManager.Instance.PlaySFXSound("Button");
-    //     LoadingManager.Instance.SceneLoad(Constants.Scene.Title);
-    // }
-    // #endregion
+    [SerializeField] Button homeBtn;
 
 
+    public void Show()
+    {
+        ShowLayer();
+    }
+    protected override void OnFirstShow()
+    {
+        // homeBtn.onClick.AddListener(OnClick_HomeBtn);
+    }
+    protected override void OnShow()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+    }
+
+    #region Event
+    private void OnClick_AttackBtn()
+    {
+        // SoundManager.Instance.PlaySFXSound("Button");
+    }
+    private void OnClick_HomeBtn()
+    {
+        // SoundManager.Instance.PlaySFXSound("Button");
+        LoadingManager.Instance.SceneLoad(Constants.Scene.Title);
+    }
+    #endregion
 }
