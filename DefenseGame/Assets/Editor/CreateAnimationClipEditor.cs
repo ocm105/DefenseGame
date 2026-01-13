be81334d968ac7512ee50a5857db7d0d9eb6d9fa
woocfgzn <woocfgzn@naver.com>
2026-01-13 22:57:49 +0900

    에셋 추가

--- DefenseGame/Assets/Editor/CreateAnimationClipEditor.cs
+++ DefenseGame/Assets/Editor/CreateAnimationClipEditor.cs
@@ -8,11 +8,6 @@ using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
 [CustomEditor(typeof(CreateAnimationClip))]
 public class CreateAnimationClipEditor : Editor
 {
-    private bool toggleValue = false;
-    private bool first = false;
-    private bool second = false;
-    private bool third = false;
-    private int selected = 1;
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
